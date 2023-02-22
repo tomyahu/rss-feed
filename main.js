716111e0b6bf9ade42239dc2e3486794531b0fbf
tomimi <tomyahu@gmail.com>
2023-02-22 19:54:32 -0300

    test base de rss

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const app = express();
 
+var titulo = 'RSS Test del Tomimi';
+
 app.get('/', ( req, res ) => {
-	res.send("Welcome");
+	var rss_str = `
+<?xml version="1.0" encoding="UTF-8" ?>
+<rss version="2.0">
+
+<channel>
+`;
+	rss_str += "<title>" + titulo + "</title>";
+
+	rss_str += `
+</channel>
+</rss>
+`;
+
+	res.send(rss_str);
 });
 
 app.listen( 4000, () => {
